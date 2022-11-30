42017a2bd9077892f33d159b1533a939ea1da96a
jahnavichanapathi <chanapathijahnavi@gmail.com>
2022-11-30 16:51:32 +0530

    propsexample

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import ClassComponent from './ClassComponent'
+import PropsExample from './PropsExample'
+function App(){
+  return(
+    <section>
+    <PropsExample name="ravi" age="20"/>
+    <PropsExample name="arjun" age="21"/>
+      {/*<p>Good afternoon</p>
+      <h1>This is heading tag</h1>
+      <img src="https://www.shutterstock.com/image-photo/night-sky-stars-moon-260nw-552010999.jpg" alt="image"/>
+      <ClassComponent/>*/}
+    </section>
+    )
+}
+export default App
---
+++ src/ClassComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+class ClassComponent extends React.Component{
+	render(){
+		return(
+			<div>This is Component</div>
+
+		)
+	}
+}
+export default ClassComponent
