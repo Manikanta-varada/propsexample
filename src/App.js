541915420cb658600411a7631d919cbbaeede22d
jahnavichanapathi <chanapathijahnavi@gmail.com>
2022-12-01 16:49:25 +0530

    events props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 //import ClassComponent from './ClassComponent'
-import PropsExample from './PropsExample'
+//import PropsExample from './PropsExample'
+//import StateExample from './StateExample'
+//import FuncProps from './FuncProps'
+//import DemoExample from './DemoExample'
+import Events from './Events'
 function App(){
   return(
     <section>
-    <PropsExample name="ravi" age="20"/>
-    <PropsExample name="arjun" age="21"/>
-      {/*<p>Good afternoon</p>
-      <h1>This is heading tag</h1>
-      <img src="https://www.shutterstock.com/image-photo/night-sky-stars-moon-260nw-552010999.jpg" alt="image"/>
-      <ClassComponent/>*/}
+    <Events/>
+    {/*<DemoExample price="1st price"/>*/}
+    {/*<StateExample/>
+      <FuncProps carname="rolls royce" year="1998"/>
+        <h1>hello</h1>*/}
+        {/*<PropsExample name="nani" age="20"/>
+        < PropsExample name="arjun" age="21"/>*/}
+        {/*<p>Good afternoon</p>
+            <h1>This is heading tag</h1>
+          <img src="https://www.shutterstock.com/image-photo/night-sky-stars-moon-260nw-552010999.jpg" alt="image"/>
+        <ClassComponent/>*/}
     </section>
-    )
+  )
 }
 export default App
---
+++ src/FuncProps.js
@@ -0,0 +1,12 @@
+function FuncProps(props){
+	return(
+		<div>
+		   <h1>from functional component</h1>
+		   {props.carname}{props.year}
+
+		</div>
+
+
+		)
+}
+export default FuncProps
